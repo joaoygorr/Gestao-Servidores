3c517e7b8c4578cd2fb0005a0e3eff0775e38bda
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-03-21 18:44:21 -0400

    feat: primary commit

---
+++ src/test/java/br/com/gestaoServidores/GestaoServidoresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gestaoServidores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoServidoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

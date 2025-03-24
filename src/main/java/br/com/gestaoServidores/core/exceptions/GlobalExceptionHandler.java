9b022443ea6a8c5d49bbfb43e27d8e01a509651a
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-03-24 19:54:15 -0400

    feat: add logs

---
+++ src/main/java/br/com/gestaoServidores/core/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package br.com.gestaoServidores.core.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleGenericException(Exception ex) {
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("error", ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}

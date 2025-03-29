7acc82b1c9ab36330bb9be8209ee3d1d439fccb8
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-03-29 17:29:46 -0400

    feat: token swagger

--- src/main/java/br/com/gestaoServidores/core/configuration/OpenApiConfig.java
+++ src/main/java/br/com/gestaoServidores/core/configuration/OpenApiConfig.java
@@ -1,8 +1,10 @@
 package br.com.gestaoServidores.core.configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,11 +27,11 @@ public class OpenApiConfig {
                         .termsOfService("https://github.com/joaoygorr/Gestao-Servidores")
                         .license(new License()
                                 .name("Apache 2.0")
-                                .url("https://github.com/joaoygorr/Gestao-Servidores")));
-//                .components(new Components()
-//                        .addSecuritySchemes("bearer-key",
-//                                new SecurityScheme()
-//                                        .type(SecurityScheme.Type.HTTP)
-//                                        .scheme("bearer").bearerFormat("JWT")));
+                                .url("https://github.com/joaoygorr/Gestao-Servidores")))
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer").bearerFormat("JWT")));
     }
 }
